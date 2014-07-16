dd03e3e6b6270b1fb5aeb9500ae9181d88814f6c
Peter Merkert <p.merkert@sap.com>
2014-07-16 12:32:35 +0200

    Added extension information to the JMS server sampling satellite adapter.
    Change-Id: Ic74acef97db79347cd5cb8d930fe71ce47d800ae
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.spotter.measurement/src/org/spotter/measurement/jmsserver/JmsServerMeasurementExtension.java
+++ org.spotter.measurement/src/org/spotter/measurement/jmsserver/JmsServerMeasurementExtension.java
@@ -28,9 +28,14 @@ import org.spotter.core.measurement.IMeasurementController;
  */
 public class JmsServerMeasurementExtension extends AbstractMeasurmentExtension {
 
+	private static final String EXTENSION_DESCRIPTION = "The jmsserver sampling measurement satellite adapter is used "
+														+ "to connect to the special sampling satellites for Java Messaging "
+														+ "Service (JMS) server. They sample more than the default sampling "
+														+ "satellites.";
+	
 	@Override
 	public String getName() {
-		return "measurement.sampler.jmsserver";
+		return "measurement.satellite.adapter.sampling.jmsserver";
 	}
 
 	@Override
@@ -74,7 +79,7 @@ public class JmsServerMeasurementExtension extends AbstractMeasurmentExtension {
 		addConfigParameter(createSamplingDelayParameter());
 		addConfigParameter(createCollectorTypeParameter());
 		addConfigParameter(createServerConnectionStringParameter());
-
+		addConfigParameter(ConfigParameterDescription.createExtensionDescription(EXTENSION_DESCRIPTION));
 	}
 
 	@Override
