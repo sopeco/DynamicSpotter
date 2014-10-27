2d8c4392e1b10d4a7fba0a8250b847281be80812
Denis Knoepfle <denis.knoepfle@sap.com>
2014-10-27 16:52:44 +0100

    Add "not available" image if resource not found
    Fixed initial display position of resource shell.
    
    Change-Id: I631db1a9965681b40eaa7bc8dc5ea470fbc51861
    Signed-off-by: Denis Knoepfle <denis.knoepfle@sap.com>

--- org.spotter.eclipse.ui/src/org/spotter/eclipse/ui/view/ResultsView.java
+++ org.spotter.eclipse.ui/src/org/spotter/eclipse/ui/view/ResultsView.java
@@ -49,8 +49,10 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.ShellAdapter;
 import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -107,7 +109,6 @@ public class ResultsView extends ViewPart implements ISelectionListener {
 
 	private static final String RESULTS_VIEW_TITLE = "Results";
 	private static final String DLG_RESOURCE_TITLE = "Resource '%s' (%s)";
-	private static final int DLG_RESOURCE_TOP_MARGIN = 40;
 	private static final int RESOURCES_LIST_RATIO = 45;
 	private static final int RESOURCES_CANVAS_RATIO = 55;
 
@@ -124,6 +125,7 @@ public class ResultsView extends ViewPart implements ISelectionListener {
 	private static final String LABEL_NOT_DETECTED = "Not Detected";
 	private static final String LABEL_NO_LOOKUP = "The corresponding result could not be looked up. (erroneous analysis)";
 	private static final String LABEL_NO_INFO = "No description available.";
+	private static final String NOT_AVAILABLE_IMG_TEXT = "not available";
 
 	private static final String TAB_HIERARCHY_NAME = "Hierarchy";
 	private static final String TAB_REPORT_NAME = "Report";
@@ -345,7 +347,7 @@ public class ResultsView extends ViewPart implements ISelectionListener {
 					if (resourceShells.containsKey(resourceIdentifier)) {
 						resourceShells.get(resourceIdentifier).setFocus();
 					} else {
-						openResourcePopupShell(resourceIdentifier);
+						openResourcePopupShell(e.display, resourceIdentifier);
 					}
 				}
 			}
@@ -406,25 +408,51 @@ public class ResultsView extends ViewPart implements ISelectionListener {
 			File file = new File(resourceFile);
 			int canvasWidth = canvasRes.getBounds().width;
 			int canvasHeight = canvasRes.getBounds().height;
-			if (file.exists() && canvasWidth > 0 && canvasHeight > 0) {
-				resourceImageData = new ImageData(resourceFile);
-				int width = Math.min(canvasWidth, resourceImageData.width);
-				int height = Math.min(canvasHeight, resourceImageData.height);
-				resourceImage = new Image(canvasRes.getDisplay(), resourceImageData.scaledTo(width, height));
-			} // else {
-				// TODO: draw "not available image" picture using GC
-			// }
+			boolean isCanvasVisible = canvasWidth > 0 && canvasHeight > 0;
+
+			if (isCanvasVisible) {
+				if (file.exists()) {
+					resourceImageData = new ImageData(resourceFile);
+					int width = Math.min(canvasWidth, resourceImageData.width);
+					int height = Math.min(canvasHeight, resourceImageData.height);
+					resourceImage = new Image(canvasRes.getDisplay(), resourceImageData.scaledTo(width, height));
+				} else {
+					// draw "not available image" picture using GC
+					final Display display = canvasRes.getDisplay();
+					final Rectangle bounds = canvasRes.getBounds();
+					resourceImage = new Image(display, bounds);
+					drawNotAvailableImage(resourceImage, display, bounds);
+				}
+			}
+
 			canvasRes.setBackgroundImage(resourceImage);
 		}
 	}
 
-	private void openResourcePopupShell(final String resourceIdentifier) {
+	private void drawNotAvailableImage(Image image, Display display, Rectangle bounds) {
+		GC gc = new GC(image);
+		gc.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
+		gc.setForeground(display.getSystemColor(SWT.COLOR_RED));
+
+		gc.fillRectangle(0, 0, bounds.width, bounds.height);
+		gc.drawLine(0, 0, bounds.width, bounds.height);
+		gc.drawLine(0, bounds.height, bounds.width, 0);
+
+		final int flags = SWT.DRAW_TRANSPARENT;
+		Point textExtent = gc.textExtent(NOT_AVAILABLE_IMG_TEXT, flags);
+
+		int x = (bounds.width - textExtent.x) / 2;
+		int y = bounds.height - textExtent.y;
+		gc.drawText(NOT_AVAILABLE_IMG_TEXT, x, y, flags);
+		gc.dispose();
+	}
+
+	private void openResourcePopupShell(final Display display, final String resourceIdentifier) {
 		final Shell popupShell = new Shell(SWT.BORDER | SWT.RESIZE | SWT.CLOSE);
 		Label label = new Label(popupShell, SWT.NONE);
-		Display display = Display.getDefault();
-		Rectangle displayRect = display.getClientArea();
+		Rectangle clientArea = display.getPrimaryMonitor().getClientArea();
 
-		final Image image = new Image(display, createScaledImageData(displayRect));
+		final Image image = new Image(display, createScaledImageData(clientArea));
 		label.setImage(image);
 
 		popupShell.setLayout(new FillLayout());
@@ -433,9 +461,9 @@ public class ResultsView extends ViewPart implements ISelectionListener {
 		popupShell.setText(String.format(DLG_RESOURCE_TITLE, resourceIdentifier, projectName));
 		popupShell.pack();
 
-		Rectangle splashRect = popupShell.getBounds();
-		int x = (displayRect.width + displayRect.x - splashRect.width) / 2;
-		int y = (displayRect.height + displayRect.y - splashRect.height) / 2;
+		Rectangle shellRect = popupShell.getBounds();
+		int x = clientArea.x + (clientArea.width - shellRect.width) / 2;
+		int y = clientArea.y + (clientArea.height - shellRect.height) / 2;
 		popupShell.setLocation(x, y);
 
 		popupShell.addKeyListener(new KeyAdapter() {
@@ -458,11 +486,11 @@ public class ResultsView extends ViewPart implements ISelectionListener {
 		popupShell.open();
 	}
 
-	private ImageData createScaledImageData(Rectangle displayRect) {
+	private ImageData createScaledImageData(Rectangle clientArea) {
 		int width = resourceImageData.width;
 		int height = resourceImageData.height;
-		int maxImageWidth = displayRect.width + displayRect.x;
-		int maxImageHeight = displayRect.height + displayRect.y - DLG_RESOURCE_TOP_MARGIN;
+		int maxImageWidth = clientArea.width;
+		int maxImageHeight = clientArea.height;
 
 		int scaledWidth = Math.min(width, maxImageWidth);
 		int scaledHeight = Math.min(height, maxImageHeight);
