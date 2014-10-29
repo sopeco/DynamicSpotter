c0e645b202c5074c72f28f1963cac487b844d1ab
Denis Knoepfle <denis.knoepfle@sap.com>
2014-10-29 15:20:05 +0100

    Add method to access current problem under investigation
    Change-Id: Ic2b00664cf93bd99f0aa32640f82edb71bbb6415
    Signed-off-by: Denis Knoepfle <denis.knoepfle@sap.com>

--- org.spotter.shared/src/org/spotter/shared/status/SpotterProgress.java
+++ org.spotter.shared/src/org/spotter/shared/status/SpotterProgress.java
@@ -22,10 +22,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * Progress of a Dynamic Spotter run/job.
  * 
  * @author Alexander Wert
- *
+ * 
  */
 public class SpotterProgress {
 	private Map<String, DiagnosisProgress> problemProgressMapping;
+	private String currentProblem;
 
 	/**
 	 * Constructor.
@@ -34,19 +35,38 @@ public class SpotterProgress {
 		problemProgressMapping = new ConcurrentHashMap<>();
 	}
 
-
-
 	/**
-	 * Returns the current diagnosis progress.
-	 * @param problemName problem name specifying the corresponding diagnosis step
-	 * @return the current progress
+	 * Returns the current diagnosis progress for the given problem.
+	 * 
+	 * @param problemId
+	 *            problem id specifying the corresponding diagnosis step
+	 * @return the current progress for the given problem
 	 */
-	public DiagnosisProgress getProgress(String problemName) {
-		if (problemProgressMapping.containsKey(problemName)) {
-			return problemProgressMapping.get(problemName);
+	public DiagnosisProgress getProgress(String problemId) {
+		if (problemProgressMapping.containsKey(problemId)) {
+			return problemProgressMapping.get(problemId);
 		} else {
 			return new DiagnosisProgress("", DiagnosisStatus.PENDING, 0.0, 0L, "");
 		}
+	}
+
+	/**
+	 * Sets the problem that is currently under investigation.
+	 * 
+	 * @param problemId
+	 *            the current problem id
+	 */
+	public synchronized void setCurrentProblem(String problemId) {
+		this.currentProblem = problemId;
+	}
+
+	/**
+	 * Returns the id of the problem that is currently under investigation.
+	 * 
+	 * @return the current problem id
+	 */
+	public synchronized String getCurrentProblem() {
+		return currentProblem;
 	}
 
 	/**
